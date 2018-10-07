aa1591a5d087a2452dcbee3d9812e210dc65bd2e
Andrew McKinnon <andrewmckinnon2@gmail.com>
2018-10-07 10:25:13 -0400

    reformated scraping_lite file

--- model/utils/scraping_lite.py
+++ model/utils/scraping_lite.py
@@ -1,64 +1,35 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {
-    "collapsed": true
-   },
-   "outputs": [],
-   "source": [
-    "def parseLyrics(song): \n",
-    "    word_list = song.split()\n",
-    "    list_copy = word_list\n",
-    "    for idx in range(len(word_list)):\n",
-    "        word = word_list[idx]\n",
-    "\n",
-    "        #remove punctuation '?' and '.'\n",
-    "        if word.find('?')!= -1:\n",
-    "            word_list[idx] = word.replace(\"?\",\"\")\n",
-    "        if word.find('.') != -1:\n",
-    "            word_list[idx] = word.replace(\".\",\"\")\n",
-    "        if word.find('\\n') != -1:\n",
-    "            word_list[idx] = word.replace(\"\\n\",\"\")\n",
-    "        if word.find('\\r') != -1:\n",
-    "            word_list[idx] = word.replace(\"\\r\",\"\")\n",
-    "\n",
-    "    words_to_remove = []\n",
-    "    for word in word_list:\n",
-    "        #remove all part of song headers i.e. [Intro] or [Chorus]\n",
-    "        if word.find('[') != -1:\n",
-    "            words_to_remove.append(word)\n",
-    "        elif word.find(']') != -1:\n",
-    "            words_to_remove.append(word)\n",
-    "\n",
-    "    for word in words_to_remove:\n",
-    "        word_list.remove(word)\n",
-    "    word_set = set(word_list)\n",
-    "\n",
-    "    return word_set"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.6.0"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+
+# coding: utf-8
+
+# In[ ]:
+
+def parseLyrics(song): 
+    word_list = song.split()
+    list_copy = word_list
+    for idx in range(len(word_list)):
+        word = word_list[idx]
+
+        #remove punctuation '?' and '.'
+        if word.find('?')!= -1:
+            word_list[idx] = word.replace("?","")
+        if word.find('.') != -1:
+            word_list[idx] = word.replace(".","")
+        if word.find('\n') != -1:
+            word_list[idx] = word.replace("\n","")
+        if word.find('\r') != -1:
+            word_list[idx] = word.replace("\r","")
+
+    words_to_remove = []
+    for word in word_list:
+        #remove all part of song headers i.e. [Intro] or [Chorus]
+        if word.find('[') != -1:
+            words_to_remove.append(word)
+        elif word.find(']') != -1:
+            words_to_remove.append(word)
+
+    for word in words_to_remove:
+        word_list.remove(word)
+    word_set = set(word_list)
+
+    return word_set
+
