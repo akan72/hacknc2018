70db81fcc60da7f6f6f24170960960f7b9a42514
Alex Kan <lexokan@live.unc.edu>
2018-10-07 09:58:44 -0400

    Indends in parser.py

--- model/utils/parser.py
+++ model/utils/parser.py
@@ -30,22 +30,21 @@ def parseLyrics(song_list):
         word_set = set(word_list)
         song_lyrics_sets.append(word_set)
     
-    #format stop words as a list so we can remove stop words
-    stwords = stopwords.raw()
-    stopword_list = stwords.split()
-    
-    #Remove stop words
-    words_to_remove = []
-    for lyrics_set in song_lyrics_sets:
-        for lyric in lyrics_set:
-            if lyric in stopword_list:
-                words_to_remove.append(lyric)
+        #format stop words as a list so we can remove stop words
+        stwords = stopwords.raw()
+        stopword_list = stwords.split()
 
-    words_to_remove = set(words_to_remove)
-    for lyrics_set in song_lyrics_sets:
-        for word in words_to_remove:
-            if word in lyrics_set:
-                lyrics_set.remove(word)
-    
-    return lyrics_set
-    #return song_lyrics_sets
+        #Remove stop words
+        words_to_remove = []
+        for lyrics_set in song_lyrics_sets:
+            for lyric in lyrics_set:
+                if lyric in stopword_list:
+                    words_to_remove.append(lyric)
+
+        words_to_remove = set(words_to_remove)
+        for lyrics_set in song_lyrics_sets:
+            for word in words_to_remove:
+                if word in lyrics_set:
+                    lyrics_set.remove(word)
+
+    return song_lyrics_sets
